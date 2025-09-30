b65975d188288c9e773c8705d02a2e7779aa6da8
SeanyCampos04 <21690072@tecvalles.mx>
2025-09-30 11:30:17 -0600

    Se hizo corrección para que el buscador funcione en la vista index.blade.php

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -25,9 +25,12 @@ class CursoController extends Controller
      */
     public function index(Request $request)
     {
-        $search = $request->input('q'); // Tomamos el valor del buscador
+        $search = $request->input('q');
 
-        // Cargamos los cursos con sus relaciones necesarias
+        // Obtener el periodo activo o el último periodo (ajusta según tu lógica)
+        $periodo = Periodo::latest()->first();
+
+        // Cargar cursos con relaciones necesarias y aplicar búsqueda
         $cursos = Curso::with([
                 'periodo',
                 'instructores.user.datos_generales',
@@ -48,7 +51,7 @@ class CursoController extends Controller
             ->orderBy('id', 'desc')
             ->get();
 
-        return view('vistas.cursos.index', compact('cursos', 'search'));
+        return view('vistas.cursos.index', compact('cursos', 'search', 'periodo'));
     }
 
     public function docente_index(Request $request)
@@ -61,6 +64,7 @@ class CursoController extends Controller
         })->get();
         return view('vistas.cursos.docente.index', compact('cursos'));
     }
+
     /**
      * Show the form for creating a new resource.
      */
@@ -80,8 +84,6 @@ class CursoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-
-
     public function store(Request $request)
     {
         // Validación de formularios
@@ -191,9 +193,9 @@ class CursoController extends Controller
             'es_tics' => 'sometimes|boolean',
             'es_tutorias' => 'sometimes|boolean',
             'instructores' => 'required|array',
-            'instructores.*' => 'exists:instructores,id', // Asegúrate de que cada instructor sea válido
+            'instructores.*' => 'exists:instructores,id',
         ]);
-        //dd('prueba');
+
         // Actualizar el curso con los datos proporcionados
         $curso->update([
             'nombre' => $request->nombre,
@@ -210,17 +212,15 @@ class CursoController extends Controller
             'tipo' => $request->tipo,
             'clase' => $request->clase,
             'limite_participantes' => $request->limite_participantes,
-            'es_tics' => $request->has('es_tics'), // Si existe, se marcará como true
-            'es_tutorias' => $request->has('es_tutorias'), // Si existe, se marcará como true
+            'es_tics' => $request->has('es_tics'),
+            'es_tutorias' => $request->has('es_tutorias'),
         ]);
 
-        // Sincronizar la relación con los instructores
+        // Sincronizar instructores
         $curso->instructores()->sync($request->instructores);
 
-        // Redirigir con un mensaje de éxito
         return redirect(route('cursos.show', $curso->id))->with('success', 'Curso actualizado correctamente.');
     }
-
 
     public function terminar_curso(Curso $curso)
     {
@@ -244,26 +244,20 @@ class CursoController extends Controller
         }
     }
 
-
-
     public function generarPDF($curso_id)
     {
         try {
-            // Buscar el curso con las relaciones necesarias
             $curso = Curso::with(['instructores.user.datos_generales', 'cursos_participantes.participante.user.datos_generales'])
-                ->findOrFail($curso_id); // Lanza una excepción si el curso no se encuentra
+                ->findOrFail($curso_id);
 
-            // Asegurarse de que la fecha de creación esté en formato Carbon
             if (!$curso->created_at instanceof Carbon) {
                 $curso->created_at = Carbon::parse($curso->created_at);
             }
 
-            // Obtener todos los cursos del mismo año, ordenados por fecha de creación
             $cursosDelAnio = Curso::whereYear('created_at', $curso->created_at->format('Y'))
                 ->orderBy('created_at')
                 ->get();
 
-            // Determinar el número del curso en el año
             $numeroDelCurso = $cursosDelAnio->search(function ($c) use ($curso) {
                 return $c->id === $curso->id;
             });
@@ -274,7 +268,6 @@ class CursoController extends Controller
 
             $numeroDelCurso += 1;
 
-            // Preparar los datos para la vista del PDF
             $data = [
                 'curso' => $curso,
                 'instructores' => $curso->instructores,
@@ -282,15 +275,12 @@ class CursoController extends Controller
                 'numeroDelCurso' => $numeroDelCurso,
             ];
 
-            // Generar el PDF
             $pdf = app(PDF::class)->loadView('vistas.cursos.pdf.curso_detalle', $data);
 
             return $pdf->download('curso-' . $curso->nombre . '.pdf');
         } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            // Error si el curso no se encuentra
             return redirect()->route('cursos.index')->with('error', 'El curso especificado no existe.');
         } catch (\Exception $e) {
-            // Otros errores
             return redirect()->route('cursos.index')->with('error', 'Error al generar el PDF: ' . $e->getMessage());
         }
     }
@@ -303,105 +293,67 @@ class CursoController extends Controller
 
     public function estadisticas_show($anio)
     {
-        // Obtener los cursos para el año seleccionado
         $cursos = Curso::whereHas('periodo', function ($query) use ($anio) {
             $query->where('anio', $anio);
         })->get();
 
-        // Inicializar los contadores por trimestre
         $estadisticas = [
-            'trimestre_1' => [
-                'total_participantes' => 0,
-                'total_docente' => 0,
-                'total_profesional' => 0,
-                'total_tics' => 0,
-                'total_tutorias' => 0,
-            ],
-            'trimestre_2' => [
-                'total_participantes' => 0,
-                'total_docente' => 0,
-                'total_profesional' => 0,
-                'total_tics' => 0,
-                'total_tutorias' => 0,
-            ],
-            'trimestre_3' => [
-                'total_participantes' => 0,
-                'total_docente' => 0,
-                'total_profesional' => 0,
-                'total_tics' => 0,
-                'total_tutorias' => 0,
-            ],
-            'trimestre_4' => [
-                'total_participantes' => 0,
-                'total_docente' => 0,
-                'total_profesional' => 0,
-                'total_tics' => 0,
-                'total_tutorias' => 0,
-            ],
+            'trimestre_1' => ['total_participantes'=>0,'total_docente'=>0,'total_profesional'=>0,'total_tics'=>0,'total_tutorias'=>0],
+            'trimestre_2' => ['total_participantes'=>0,'total_docente'=>0,'total_profesional'=>0,'total_tics'=>0,'total_tutorias'=>0],
+            'trimestre_3' => ['total_participantes'=>0,'total_docente'=>0,'total_profesional'=>0,'total_tics'=>0,'total_tutorias'=>0],
+            'trimestre_4' => ['total_participantes'=>0,'total_docente'=>0,'total_profesional'=>0,'total_tics'=>0,'total_tutorias'=>0],
         ];
 
-        // Recorrer los cursos para calcular las estadísticas
         foreach ($cursos as $curso) {
-            // Obtener el trimestre del periodo
             $trimestre = $curso->periodo->trimestre;
 
-            // Contar los participantes en cursos de tipo 'Docente' y 'Profesional'
             if ($curso->clase == 'Docente') {
                 $estadisticas["trimestre_$trimestre"]['total_docente'] += $curso->participantes->count();
             } elseif ($curso->clase == 'Profesional') {
                 $estadisticas["trimestre_$trimestre"]['total_profesional'] += $curso->participantes->count();
             }
 
-            // Contar los participantes en cursos con es_tics
             if ($curso->es_tics) {
                 $estadisticas["trimestre_$trimestre"]['total_tics'] += $curso->participantes->count();
             }
 
-            // Contar los participantes en cursos con es_tutorias
             if ($curso->es_tutorias) {
                 $estadisticas["trimestre_$trimestre"]['total_tutorias'] += $curso->participantes->count();
             }
         }
 
         for ($i = 1; $i <= 4; $i++) {
-            // Definir las fechas completas de inicio y fin del trimestre actual
             $fecha_inicio_trimestre = sprintf('%d-%02d-01', $anio, (($i - 1) * 3) + 1);
             $fecha_fin_trimestre = sprintf('%d-%02d-%02d', $anio, $i * 3, cal_days_in_month(CAL_GREGORIAN, $i * 3, $anio));
 
-            // Obtener los usuarios con tipo 1 y estatus 1 para el trimestre actual
             $usuariosTipo1Estatus1Trimestre = DB::table('users')
                 ->join('historial_usuarios', 'users.id', '=', 'historial_usuarios.user_id')
                 ->where('historial_usuarios.tipo', 1)
                 ->where('historial_usuarios.estatus', 1)
                 ->where(function ($query) use ($fecha_inicio_trimestre, $fecha_fin_trimestre) {
-                    // Verificar que el registro intersecta el trimestre actual
                     $query->where(function ($subQuery) use ($fecha_inicio_trimestre, $fecha_fin_trimestre) {
-                        // Caso 1: Registros con fecha_fin definida
                         $subQuery->whereNotNull('historial_usuarios.fecha_fin')
                             ->whereDate('historial_usuarios.fecha_inicio', '<=', $fecha_fin_trimestre)
                             ->whereDate('historial_usuarios.fecha_fin', '>=', $fecha_inicio_trimestre);
                     })
                         ->orWhere(function ($subQuery) use ($fecha_fin_trimestre) {
-                            // Caso 2: Registros con fecha_fin nula (activos)
                             $subQuery->whereNull('historial_usuarios.fecha_fin')
                                 ->whereDate('historial_usuarios.fecha_inicio', '<=', $fecha_fin_trimestre);
                         });
                 })
-                ->distinct('users.id') // Evitar duplicados
+                ->distinct('users.id')
                 ->count();
 
-            // Acumular los valores por trimestre
             $estadisticas["trimestre_$i"]['total_participantes'] += $usuariosTipo1Estatus1Trimestre;
         }
 
-        // Pasar las estadísticas a la vista
         return view('vistas.cursos.admin.estadisticas.show', compact('estadisticas', 'anio'));
     }
 
     public function entregar_calificaciones($id)
     {
         try {
-            $curso = Curso::findOrFail($id); // Lanza una excepción si no encuentra el curso
+            $curso = Curso::findOrFail($id);
             $curso->estado_calificacion = 2;
             $curso->save();
 
@@ -414,7 +366,7 @@ class CursoController extends Controller
     public function devolver_calificaciones($id)
     {
         try {
-            $curso = Curso::findOrFail($id); // Lanza una excepción si no encuentra el curso
+            $curso = Curso::findOrFail($id);
             $curso->estado_calificacion = 0;
             $curso->save();
 
